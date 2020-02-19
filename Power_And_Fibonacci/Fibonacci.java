31e0fa01bafd22fa05eaf731ee0a1cce1e6c1d34
Or Mendel <57266329+Ormendel@users.noreply.github.com>
2020-02-19 21:07:33 +0200

    Fixing multiply matrixes
    Now can work when A is matrix m x n, B is matrix n x k, so therefore C will be m x k

--- Power_And_Fibonacci/Fibonacci.java
+++ Power_And_Fibonacci/Fibonacci.java
@@ -4,19 +4,24 @@ import java.util.Arrays;
 
 public class Fibonacci
 {
-	
-	//This method can only be used for matrixes from size n x n
+
+	/**This function calculates multiply of one matrix in another
+	 * 
+	 * @param firstMatrix - can be n x n or m x k
+	 * @param secondMatrix can be n x n or k x q
+	 * @return thirdMatrix from n x n or m x q
+	 */
 	public static int[][] multiplyMatrixes(int[][] firstMatrix, int[][] secondMatrix)
 	{
-		//The size of product doesn't matter, hence firstMatrix equals by size to secondMatrix
-		int[][] product = new int[firstMatrix.length][firstMatrix[0].length];
+		if(firstMatrix[0].length!=secondMatrix.length)
+			return null;
+		int[][]product = new int[firstMatrix.length][secondMatrix[0].length];
 		for (int i = 0; i < product.length; i++) 
 		{
 			int[]row=firstMatrix[i];
 			int sum=0;
 			int index_row=0;
-			//Building one size array that represents a column in secondMatrix
-			for(int j=0;j<product[0].length;j++)
+			for(int j=0;j<secondMatrix[0].length;j++)
 			{
 				for(int val:row)
 				{
@@ -29,32 +34,33 @@ public class Fibonacci
 		}
 		return product;
 	}
-	
+
 	public static int MatrixFibi(int n) 
 	{
 		int ans[][] = { { 1, 1 }, { 1, 0 } };
 		int A[][] = { { 1, 1 }, { 1, 0 } };
 		n -= 2;
-		while (n != 0) {
+		while (n != 0)
+		{
 			if(n%2 == 1)
 				ans = multiplyMatrixes(ans, A);
 			A = multiplyMatrixes(A,A);
 			n = n/2;
 		}
-		return ans[0][0];
+		return ans[0][0];//Our answer is located here
 	}
 	public static void main(String[] args)
 	{
-		int[][]m1= new int[][]{{1,4,3},{8,3,1},{3,3,17}};
-		int[][]m2= new int[][]{{2,1,3},{5,1,5},{2,2,9}};
+		int[][]m1= new int[][]{{8,5,8,4}};
+		int[][]m2= new int[][]{{8},{8},{8},{8}};
 		int[][]m3=multiplyMatrixes(m1,m2);
 		for(int i=0;i<m3.length;i++)
 			System.out.println(Arrays.toString(m3[i]));
-		
+
 		System.out.println();
 		System.out.println(MatrixFibi(6));
-		
-		
+
+
 	}
 
 }
