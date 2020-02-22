c483f18b3849948e88ca37174dd0617a863606af
Or Mendel <57266329+Ormendel@users.noreply.github.com>
2020-02-22 23:57:14 +0200

    Airplane problem**
    Need to check and understand the code

---
+++ Airplane_Problem/Node.java
@@ -0,0 +1,21 @@
+package Airplane_Problem;
+
+public class Node 
+{
+
+	int x, y, price, price2, numOfPaths;
+
+	public Node(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+		this.price = 0;
+		this.price2 = 0;
+		this.numOfPaths = 0;
+	}
+
+	public String toString()
+	{
+		return "(" + x + "," + y + ") ";
+	}
+}
